206917d9de0638f31c61509270131b166ad59a42
jsivieri <37198083+jsivieri@users.noreply.github.com>
2023-03-26 09:51:21 -0300

    Aula 4

---
+++ aula4/src/aula4/Testar.java
@@ -0,0 +1,18 @@
+package aula4;
+
+public class Testar {
+
+	public static void main(String[] args, Aluno Pessoa) {
+		
+		
+		Pessoa Aluno = new Pessoa();
+		Aluno aluno = Pessoa;
+		aluno.setNome("Maria");
+		aluno.setPeriodo(5);
+		
+
+	
+
+	}
+
+}
